1ed0105a7a65253a357a5c3cb14e51db382a130e
AbhilashIyer <i.abhilash90@gmail.com>
2019-07-08 09:25:32 -0700

    JUly 8_update
    Former-commit-id: b6caa217c8b0f22bea7a4129193586e3ed370ccb

--- microphone.py
+++ microphone.py
@@ -17,19 +17,18 @@ import vggish_params
 import vggish_input
 import vggish_postprocess
 
-import numpy
 import librosa 
 import os
 
 import numpy as np
 from scipy.io import wavfile
 import six
-import vggish_slim
-import vggish_params
-import vggish_input
 
 import tensorflow as tf
 import keras
+from keras.preprocessing.sequence import pad_sequences
+import itertools
+from keras.models import load_model
 
 from sys import byteorder
 from array import array
@@ -43,89 +42,26 @@ CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
 RATE = 44100
 
+flags = tf.app.flags
 
-#Creating a VGGish network
-def CreateVGGishNetwork(hop_size=0.96):   # Hop size is in seconds.
-  """Define VGGish model, load the checkpoint, and return a dictionary that points
-  to the different tensors defined by the model.
-  """
-  vggish_slim.define_vggish_slim()
-  checkpoint_path = 'src/audioset_VGG/vggish_model.ckpt' #'audioset_VGG/vggish_model.ckpt'
-  vggish_params.EXAMPLE_HOP_SECONDS = hop_size
-  
-  vggish_slim.load_vggish_slim_checkpoint(sess, checkpoint_path)
+flags.DEFINE_string(
+    'wav_file', None,
+    'Path to a wav file. Should contain signed 16-bit PCM samples. '
+    'If none is provided, a synthetic sound is used.')
 
-  features_tensor = sess.graph.get_tensor_by_name(vggish_params.INPUT_TENSOR_NAME)
-  embedding_tensor = sess.graph.get_tensor_by_name(vggish_params.OUTPUT_TENSOR_NAME)
+flags.DEFINE_string(
+    'checkpoint', 'src/VGGish/vggish_model.ckpt',
+    'Path to the VGGish checkpoint file.')
 
-  layers = {'conv1': 'vggish/conv1/Relu',
-            'pool1': 'vggish/pool1/MaxPool',
-            'conv2': 'vggish/conv2/Relu',
-            'pool2': 'vggish/pool2/MaxPool',
-            'conv3': 'vggish/conv3/conv3_2/Relu',
-            'pool3': 'vggish/pool3/MaxPool',
-            'conv4': 'vggish/conv4/conv4_2/Relu',
-            'pool4': 'vggish/pool4/MaxPool',
-            'fc1': 'vggish/fc1/fc1_2/Relu',
-            'fc2': 'vggish/fc2/Relu',
-            'embedding': 'vggish/embedding',
-            'features': 'vggish/input_features',
-         }
-  g = tf.get_default_graph()
-  for k in layers:
-    layers[k] = g.get_tensor_by_name( layers[k] + ':0')
-    
-  return {'features': features_tensor,
-          'embedding': embedding_tensor,
-          'layers': layers,
-         }
+flags.DEFINE_string(
+    'pca_params', 'src/VGGish/vggish_pca_params.npz',
+    'Path to the VGGish PCA parameters file.')
 
+flags.DEFINE_string(
+    'tfrecord_file', None,
+    'Path to a TFRecord file where embeddings will be written.')
 
-# Post Process with VGGish output by applying a PCA transfromation and as well 
-# as quantization to 8 bits per embedding element. 
-
-def ProcessWithVGGish(vgg, x, sr):
-  '''Run the VGGish model, starting with a sound (x) at sample rate
-  (sr). Return a whitened version of the embeddings. Sound must be scaled to be
-  floats between -1 and +1.'''
-
-  # Produce a batch of log mel spectrogram examples.
-  input_batch = vggish_input.waveform_to_examples(x, sr)
-  # print('Log Mel Spectrogram example: ', input_batch[0])
-
-  [embedding_batch] = sess.run([vgg['embedding']],feed_dict={vgg['features']: input_batch})
-
-  # Postprocess the results to produce whitened quantized embeddings.
-  pca_params_path = 'src/audioset_VGG/vggish_pca_params.npz' #'audioset_VGG/vggish_pca_params.npz'
-
-  pproc = vggish_postprocess.Postprocessor(pca_params_path)
-  postprocessed_batch = pproc.postprocess(embedding_batch)
-  # print('Postprocessed VGGish embedding: ', postprocessed_batch[0])
-  return postprocessed_batch[0]
-
-
-def EmbeddingsFromVGGish(vgg, x, sr):
-  '''Run the VGGish model, starting with a sound (x) at sample rate
-  (sr). Return a dictionary of embeddings from the different layers
-  of the model.'''
-  # Produce a batch of log mel spectrogram examples.
-  input_batch = vggish_input.waveform_to_examples(x, sr)
-  # print('Log Mel Spectrogram example: ', input_batch[0])
-
-  layer_names = vgg['layers'].keys()
-  tensors = [vgg['layers'][k] for k in layer_names]
-  
-  results = sess.run(tensors,
-                     feed_dict={vgg['features']: input_batch})
-
-  resdict = {}
-  for i, k in enumerate(layer_names):
-    resdict[k] = results[i]
-    
-  return resdict
-
-
-
+FLAGS = flags.FLAGS
 
 def is_silent(snd_data):
     "Returns 'True' if below the 'silent' threshold"
@@ -135,7 +71,6 @@ def normalize(snd_data):
     "Average the volume out"
     MAXIMUM = 16384
     times = float(MAXIMUM)/max(abs(i) for i in snd_data)
-
     r = array('h')
     for i in snd_data:
         r.append(int(i*times))
@@ -215,8 +150,8 @@ def record():
     p.terminate()
 
     r = normalize(r)
-    # r = trim(r)
-    #r = add_silence(r, 0.5)
+    r = trim(r)
+    r = add_silence(r, 0.5)
     return sample_width, r
 
 def record_to_file(path):
@@ -232,28 +167,101 @@ def record_to_file(path):
     wf.close()
     # infer()
 
+
+def main(_):
+
+  print("please speak a word into the microphone")
+  record_to_file('demo.wav')
+
+  y,sr = librosa.load('demo.wav')
+  print("sampling rate:",  sr)
+  print("Recorded sound wave: ")
+  print(sr)
+
+  wav_file = 'demo.wav'
+  examples_batch = vggish_input.wavfile_to_examples(wav_file)
+  # print(examples_batch)
+  # Prepare a postprocessor to munge the model embeddings.
+  pproc = vggish_postprocess.Postprocessor(FLAGS.pca_params)
+
+  # If needed, prepare a record writer to store the postprocessed embeddings.
+  writer = tf.python_io.TFRecordWriter(FLAGS.tfrecord_file) if FLAGS.tfrecord_file else None
+
+  with tf.Graph().as_default(), tf.Session() as sess:
+    # Define the model in inference mode, load the checkpoint, and
+    # locate input and output tensors.
+    vggish_slim.define_vggish_slim(training=False)
+    vggish_slim.load_vggish_slim_checkpoint(sess, FLAGS.checkpoint)
+    features_tensor = sess.graph.get_tensor_by_name(
+        vggish_params.INPUT_TENSOR_NAME)
+    embedding_tensor = sess.graph.get_tensor_by_name(
+        vggish_params.OUTPUT_TENSOR_NAME)
+
+    # Run inference and postprocessing.
+    [embedding_batch] = sess.run([embedding_tensor],
+                                 feed_dict={features_tensor: examples_batch})
+    # print(embedding_batch)
+    postprocessed_batch = pproc.postprocess(embedding_batch)
+    # print(postprocessed_batch)
+
+    # Write the postprocessed embeddings as a SequenceExample, in a similar
+    # format as the features released in AudioSet. Each row of the batch of
+    # embeddings corresponds to roughly a second of audio (96 10ms frames), and
+    # the rows are written as a sequence of bytes-valued features, where each
+    # feature value contains the 128 bytes of the whitened quantized embedding.
+    tf_seq_example = tf.train.SequenceExample(
+        feature_lists=tf.train.FeatureLists(
+            feature_list={
+                vggish_params.AUDIO_EMBEDDING_FEATURE_NAME:
+                    tf.train.FeatureList(
+                        feature=[
+                            tf.train.Feature(
+                                bytes_list=tf.train.BytesList(
+                                    value=[embedding.tobytes()]))
+                            for embedding in postprocessed_batch
+                        ]
+                    )
+            }
+        )
+    )
+    # print(tf_seq_example)
+    if writer:
+      writer.write(tf_seq_example.SerializeToString())
+
+  if writer:
+    writer.close()
+
+  X=[]
+  max_len=10
+  n_frames = len(tf_seq_example.feature_lists.feature_list['audio_embedding'].feature)
+  # print("number of frames = ", n_frames)
+
+  audio_frame = []
+  for i in range(n_frames):
+      audio_frame.append(np.frombuffer(tf_seq_example.feature_lists.feature_list['audio_embedding'].feature[i].bytes_list.value[0],np.uint8).astype(np.float32))
+  
+
+  pad = [np.zeros([128], np.float32) for i in range(max_len-n_frames)]
+  audio_frame += pad
+  X.append(audio_frame)
+
+  X = np.array(X)
+  # print("Dimension before adding newaxis", X.shape)
+
+  # X = X[newaxis,:,:]
+  # print("Dimension after adding newaxis", X.shape)
+
+  #Loading LSTM model
+  m4 = load_model('src/models/1LayerLSTM__Loss=BinCE_20Epochs_july02.h5')
+  p4 = m4.predict(X)
+
+  print("Gunshot score for inference_sample: ====> ", float(p4*100),"percent confidence")
+  if(p4>= 0.51):
+    print("Gunshot present in the clip")
+  else:
+    print("Gunshot is not present in the clip")
+
+
+
 if __name__ == '__main__':
-    print("please speak a word into the microphone")
-    record_to_file('demo.wav')
-
-    y,sr = librosa.load('demo.wav')
-    print("sampling rate:",  sr)
-    print("Recorded sound wave: ")
-    print(sr)
-
-    tf.reset_default_graph()
-    sess = tf.Session()
-
-    vgg = CreateVGGishNetwork()
-
-    resdict = EmbeddingsFromVGGish(vgg, y,sr)
-    for k in resdict:
-      print(k, resdict[k].shape)
-    print("Embedding", resdict['embedding'])
-
-    model = keras.models.load_model('src/Models/1LayerLSTM__Loss=BinCE_40Epochs_highestacc.h5')
-    p = model.predict(np.expand_dims(resdict['embedding'], axis=0))
-    print("Gunshot prediction probability", p*100)
-
-
-    print("--- %s seconds ---" % (time.time() - start_time))
+  tf.app.run()
